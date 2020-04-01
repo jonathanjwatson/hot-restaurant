37d5a4eb70b15b7fb6fe50611185a42e1855b059
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-04-01 13:21:04 -0400

    Created basic html, and basic server with pseudocoded routes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Route to serve index.html
+
+// Route to serve reservations.html
+
+// Route to serve new.html
+
+// Route to serve JSON of the list of reservations
+
+// Route to serve JSON of the list of wait-listed reservations
+
+// Route to add new reservation
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
